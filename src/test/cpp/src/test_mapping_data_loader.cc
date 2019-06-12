4a549ab6239897c61c74f9829212659d5232c7d3
nalinigans <Nalini Ganapati>
2019-06-11 17:08:04 -0700

    Move GenomicsDB C/C++ tests to use Catch2 instead of Google Test. It's   Header only   No need for fixtures   Really nice expression support for matchers.

--- src/test/cpp/src/test_mapping_data_loader.cc
+++ src/test/cpp/src/test_mapping_data_loader.cc
@@ -1,6 +1,7 @@
 /**
  * The MIT License (MIT)
  * Copyright (c) 2016 Intel Corporation
+ * Copyright (c) 2019 Omics Data Automation, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of 
  * this software and associated documentation files (the "Software"), to deal in 
@@ -18,7 +19,9 @@
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ */
+
+#include <catch2/catch.hpp>
 
 #include <test_mapping_data_loader.h>
 
@@ -51,7 +54,7 @@ void MappingDataLoaderTester::validate_contig_info() {
   for (auto& contig : m_contig_idx_to_info) {
     std::cout <<contig.m_contig_idx <<" - " <<contig.m_name <<" - " 
       <<contig.m_tiledb_column_offset <<" - " <<contig.m_length <<"\n";
-    EXPECT_GT(contig.m_tiledb_column_offset, prev_offset);
+    CHECK(contig.m_tiledb_column_offset > prev_offset);
     prev_offset = contig.m_tiledb_column_offset;
   }
 
@@ -60,7 +63,7 @@ void MappingDataLoaderTester::validate_contig_info() {
 
   for (auto& contig_begin : m_contig_begin_2_idx) {
     std::cout <<contig_begin.first <<" - " <<contig_begin.second <<"\n";
-    EXPECT_GT(contig_begin.first, prev_begin_first);
+    CHECK(contig_begin.first > prev_begin_first);
     prev_begin_first = contig_begin.first;
   }
 
@@ -69,7 +72,7 @@ void MappingDataLoaderTester::validate_contig_info() {
 
   for (auto& contig_end : m_contig_end_2_idx) {
     std::cout <<contig_end.first <<" - " <<contig_end.second <<"\n";
-    EXPECT_GT(contig_end.first, prev_end_first);
+    CHECK(contig_end.first > prev_end_first);
     prev_end_first = contig_end.first;
   }
 
@@ -84,8 +87,8 @@ void MappingDataLoaderTester::validate_callset_info() {
   for (std::vector<CallSetInfo>::iterator it = m_row_idx_to_info.begin(); it != m_row_idx_to_info.end(); ++it) {
     index++;
     std::cout <<it->m_row_idx <<" - " <<it->m_name <<" - " <<it->m_file_idx <<" - " <<it->m_idx_in_file <<"\n";
-    EXPECT_EQ(index, it->m_row_idx);
-    EXPECT_EQ(m_callset_name_to_row_idx[it->m_name], index);
+    REQUIRE(index == it->m_row_idx);
+    CHECK(m_callset_name_to_row_idx[it->m_name] == index);
   }
 
   std::cout <<"------------------------------------------------------\n";
@@ -102,8 +105,8 @@ void MappingDataLoaderTester::validate_field_info() {
       <<field.m_bcf_ht_type <<" - " <<field.m_is_vcf_FILTER_field <<" - " 
       <<field.m_is_vcf_INFO_field <<" - " <<field.m_is_vcf_FORMAT_field <<" - "
       <<field.m_VCF_field_combine_operation <<"\n";
-    EXPECT_EQ(++index, field.m_field_idx);
-    EXPECT_EQ(m_field_name_to_idx[field.m_name], index);
+    REQUIRE(++index == field.m_field_idx);
+    CHECK(m_field_name_to_idx[field.m_name] == index);
   }
 
   std::cout <<"------------------------------------------------------\n";
@@ -112,14 +115,14 @@ void MappingDataLoaderTester::validate_field_info() {
 
 MappingDataLoaderTester* SQLMapperTest::loaderTester = NULL;
 
-TEST_F(SQLMapperTest, ValidContigInfo) {
+TEST_CASE_METHOD(SQLMapperTest, "Test valid contig info", test_valid_contig_info) {
   loaderTester->validate_contig_info();
 }
 
-TEST_F(SQLMapperTest, ValidCallSetInfo) {
+TEST_CASE_METHOD(SQLMapperTest, "Test valid callset info", test_valid_callset_info) {
   loaderTester->validate_callset_info();
 }
 
-TEST_F(SQLMapperTest, ValidFieldInfo) {
+TEST_CASE_METHOD(SQLMapperTest, "Test valid field info", test_valid_field_info) {
   loaderTester->validate_field_info();
 }
