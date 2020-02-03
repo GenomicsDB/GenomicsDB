3765be310c2aa5838c6ba706a1e39982fc69a89f
Nalini Ganapati <35076948+nalinigans@users.noreply.github.com>
2020-02-02 20:28:49 -0800

    Change names of built tools to be *genomicsdb* (#78)
    Change names of built tools to be *genomicsdb*

--- tests/run_spark_hdfs.py
+++ tests/run_spark_hdfs.py
@@ -374,10 +374,10 @@ def main():
             with open(loader_json_filename, 'wb') as fptr:
                 json.dump(test_loader_dict, fptr, indent=4, separators=(',', ': '));
                 fptr.close();
-            # invoke vcf2tiledb -r <rank> where <rank> goes from 0 to num partitions
+            # invoke vcf2genomicsdb -r <rank> where <rank> goes from 0 to num partitions
             # otherwise this only loads the first partition
             for i in range(0, len(col_part)):
-                etl_cmd=exe_path+os.path.sep+'vcf2tiledb -r '+str(i)+' '+loader_json_filename
+                etl_cmd=exe_path+os.path.sep+'vcf2genomicsdb -r '+str(i)+' '+loader_json_filename
                 pid = subprocess.Popen(etl_cmd, shell=True,
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE);
                 stdout_string, stderr_string = pid.communicate()
