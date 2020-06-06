dde48e1fe2fbf67fb1283a3147adfed4c998f10f
nalinigans <nalini@omicsautomation.com>
2020-06-05 23:14:36 -0700

    Explicitly add EnableSharedPosixfsOptimizations to ImportConfiguration

--- src/main/cpp/src/vcf/vcf2binary.cc
+++ src/main/cpp/src/vcf/vcf2binary.cc
@@ -160,7 +160,7 @@ void VCFReader::add_reader() {
   assert(m_fptr == 0);  //normal file handle should be NULL
   errno = 0;
   if (bcf_sr_add_reader(m_indexed_reader, m_name.c_str()) != 1) {
-    std::string errmsg =  (errno>0) ? logger.format(" errno={1} ({2})", errno, std::strerror(errno))
+    std::string errmsg =  (errno>0) ? logger.format(" errno={} ({})", errno, std::strerror(errno))
         : " (VCF/BCF files must be block compressed and indexed)";
     throw VCF2BinaryException(std::string("Could not open file ")+m_name+" : "
                               + bcf_sr_strerror(m_indexed_reader->errnum) + errmsg);
