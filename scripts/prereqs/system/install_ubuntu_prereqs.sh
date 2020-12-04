d9ca317fd766d1e0b0c74705d2a7bd8374c0377d
Nalini Ganapati <35076948+nalinigans@users.noreply.github.com>
2020-12-04 14:47:35 -0800

    GitHub Actions to build and test GenomicsDB (#130)
    * GitHub Actions to build and test GenomicsDB

--- scripts/prereqs/system/install_ubuntu_prereqs.sh
+++ scripts/prereqs/system/install_ubuntu_prereqs.sh
@@ -23,62 +23,102 @@
 
 set -e
 
-install_openjdk8() {
-	apt-get -y install openjdk-8-jdk icedtea-plugin &&
-		apt-get update -q &&
-		pushd $HOME &&
-		git clone https://github.com/michaelklishin/jdk_switcher.git &&
-		source jdk_switcher/jdk_switcher.sh && jdk_switcher use openjdk8 &&
-		echo "export JAVA_HOME=$JAVA_HOME" >> $PREREQS_ENV &&
-		popd
+install_jdk8() {
+  if type -p javac; then
+    JAVAC=java
+  elif [[ -n $JAVA_HOME ]] && [[ -x $JAVA_HOME/bin/javac ]];  then
+    JAVAC="$JAVA_HOME/bin/java"
+  fi
+  if [[ ! -z $JAVAC ]]; then
+    JDK_VERSION=$($JAVAC -version 2>&1 | awk '/version/{print $2}')
+  fi
+  if [[ -z $JDK_VERSION || $JDK_VERSION < "1.8" ]]; then
+    apt-get -y install openjdk-8-jdk icedtea-plugin &&
+      apt-get update -q &&
+      pushd $HOME &&
+      git clone https://github.com/michaelklishin/jdk_switcher.git &&
+      source jdk_switcher/jdk_switcher.sh && jdk_switcher use openjdk8 &&
+      echo "export JAVA_HOME=$JAVA_HOME" >> $PREREQS_ENV &&
+      popd
+  fi
 }
 
 install_cmake3() {
-	wget -nv https://github.com/Kitware/CMake/releases/download/v3.4.0/cmake-3.4.0-Linux-x86_64.sh -P /tmp &&
-		chmod +x /tmp/cmake-3.4.0-Linux-x86_64.sh &&
-		/tmp/cmake-3.4.0-Linux-x86_64.sh --prefix=/usr/local --skip-license
+  CMAKE=`which cmake`
+  if [[ ! -z $CMAKE ]]; then
+    CMAKE_VERSION=$($CMAKE -version | awk '/version/{print $3}')
+  fi
+  if [[ -z $CMAKE_VERSION || CMAKE_VERSION < "3.6" ]]; then
+    echo "Installing cmake..."
+    wget -nv https://github.com/Kitware/CMake/releases/download/v3.19.1/cmake-3.19.1-Linux-x86_64.sh -P /tmp &&
+      chmod +x /tmp/cmake-3.19.1-Linux-x86_64.sh &&
+      /tmp/cmake-3.19.1-Linux-x86_64.sh --prefix=/usr/local --skip-license
+  fi
+}
+
+install_gcc() {
+  GCC=`which gcc`
+  GPLUSPLUS=`which g++`
+  if [[ -z $GCC || -z $GPLUSPLUS ]]; then
+    apt-get -y install build-essential software-properties-common
+  fi
+}
+
+install_package() {
+  if [[ $# -ne 2 ]]; then
+    dpkg -s $1 &> /dev/null
+  else
+    echo "Checking if executable exists"
+    which $1
+  fi
+  if [ $? -ne 0 ]; then
+    echo "Installing $1..."
+    apt-get install $1
+    echo "Installing DONE"
+  fi
 }
 
 install_R() {
+  echo "NYI: install R functionality"
+  return 0
   apt-get -y install gnupg &&
   apt-get install -y software-properties-common &&
   apt-get update -q &&
-	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 &&
-	add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/' &&
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 &&
+  add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/' &&
   apt-get -y install libxml2-dev &&
-	apt-get update -q &&
-	apt-get -y install r-base
+  apt-get update -q &&
+  apt-get -y install r-base
 }
 
-install_prerequisites_ubuntu() {
-	apt-get update -q &&
-    apt-get install -y tzdata &&
-		apt-get -y install \
-						lcov \
-						mpich \
-						zlib1g-dev \
-						libssl-dev \
-						rsync \
-						libidn11 \
-						uuid-dev \
-						libcurl4-openssl-dev \
-						build-essential \
-						software-properties-common \
-						wget \
-						git \
-						autoconf \
-						automake \
-						libtool \
-						zip \
-						unzip \
-						curl \
-						sudo &&
-		apt-get update -q &&
-		apt-get update -q &&
-		install_openjdk8 &&
-		install_R &&
-		apt-get clean &&
-		apt-get purge -y &&
-		install_cmake3 &&
-		rm -rf /var/lib/apt/lists*
+install_system_prerequisites() {
+  apt-get update -q &&
+    install_package tzdata &&
+    install_package install 1 &&
+    install_package lcov 1 &&
+    install_package mpich &&
+    install_package zlib1g-dev &&
+    install_package libssl-dev &&
+    install_package rsync 1 &&
+    install_package libidn11 &&
+    install_package uuid-dev &&
+    install_package libcurl4-openssl-dev &&
+    install_package wget 1 &&
+    install_package autoconf 1 &&
+    install_package automake 1 &&
+    install_package libtool 1 &&
+    install_package zip 1 &&
+    install_package unzip 1 &&
+    install_package curl 1 &&
+    install_package git &&
+    install_package libcsv-dev &&
+    install_package sudo 1 &&
+    apt-get update -q &&
+    install_gcc &&
+    install_jdk8 &&
+    install_cmake3 &&
+    install_R &&
+    apt-get clean &&
+    apt-get purge -y &&
+    rm -rf /var/lib/apt/lists*
 }
