2b2b8e7e10c2f594795e7b7d05077fd67e416c54
Nalini Ganapati <35076948+nalinigans@users.noreply.github.com>
2021-03-19 16:53:09 -0700

    Fixes to work with newer versions of cmake and other dependencies. (#143)
    * Fix FindGPerftools.make to work with the newer versions of cmake and add -Wno-expansion-to-defined -Wno-nullability-completeness to macos builds to suppress the annoying pages and pages of nullability warnings while compiling htslib
    
    * Add --without zstd to build curl for now. Can revisit this when we allow for compressions other than zlib from GenomicsDB

--- cmake/Modules/Findhtslib.cmake
+++ cmake/Modules/Findhtslib.cmake
@@ -44,9 +44,15 @@ endif()
 
 #Build if htslib source directory specified
 if(HTSLIB_SOURCE_DIR)
-    set(HTSLIB_Debug_CFLAGS "-Wall -fPIC -DDEBUG  -g3 -gdwarf-3 -DVCF_ALLOW_INT64=1")  #will be picked if compiling in debug mode
+    set(HTSLIB_Debug_CFLAGS " -Wall -fPIC -DDEBUG  -g3 -gdwarf-3 -DVCF_ALLOW_INT64=1")  #will be picked if compiling in debug mode
+    if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+        set(HTSLIB_Debug_CFLAGS "${HTSLIB_Debug_CFLAGS} -Wno-expansion-to-defined -Wno-nullability-completeness")
+    endif()
     set(HTSLIB_Coverage_CFLAGS "${HTSLIB_Debug_CFLAGS}")
     set(HTSLIB_Release_CFLAGS " -Wall -fPIC -O3 -DVCF_ALLOW_INT64=1")
+    if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+        set(HTSLIB_Release_CFLAGS "${HTSLIB_Release_CFLAGS} -Wno-expansion-to-defined -Wno-nullability-completeness")
+    endif()
     set(HTSLIB_Debug_LDFLAGS "-g3 -gdwarf-3")
     set(HTSLIB_Coverage_LDFLAGS "${HTSLIB_Debug_LDFLAGS}")
     set(HTSLIB_Release_LDFLAGS "")
