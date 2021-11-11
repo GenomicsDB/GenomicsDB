e61e380cfe2dbc59c4e20ef6b77301e6918c381f
Andrei Blebea <andrei@qc-3.oda-internal.com>
2021-11-11 10:47:33 -0800

    Commented out part of test_tools

--- tests/test_tools.sh
+++ tests/test_tools.sh
@@ -332,17 +332,17 @@ run_command "gt_mpi_gather -l $WORKSPACE/loader.json -j $QUERY_JSON --produce-Br
 create_sample_list t0_1_2_combined.vcf.gz
 run_command_and_check_results "vcf2genomicsdb_init -w $WORKSPACE -s $SAMPLE_LIST -o" 3 85 18 85 "#31"
 #run_command "gt_mpi_gather -l $WORKSPACE/loader.json -j $QUERY_JSON --produce-bgen"
-cat $QUERY_JSON
-echo ===========================
-ls $WORKSPACE
-echo ===========================
-cat $WORKSPACE/vidmap.json
-echo ===========================
-cat $WORKSPACE/callset.json
-echo ===========================
-cat $WORKSPACE/loader.json
-echo ===========================
-run_command "gt_mpi_gather -j $QUERY_JSON --produce-bgen"
+#cat $QUERY_JSON
+#echo ===========================
+#ls $WORKSPACE
+#echo ===========================
+#cat $WORKSPACE/vidmap.json
+#echo ===========================
+#cat $WORKSPACE/callset.json
+#echo ===========================
+#cat $WORKSPACE/loader.json
+#echo ===========================
+#run_command "gt_mpi_gather -j $QUERY_JSON --produce-bgen"
 
 cleanup
 exit $STATUS
