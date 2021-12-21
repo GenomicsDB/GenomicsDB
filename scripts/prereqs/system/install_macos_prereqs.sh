c45768d5553a6cc782d287929e68ba943ac16523
Nalini Ganapati <35076948+nalinigans@users.noreply.github.com>
2021-12-21 09:49:25 -0800

    Upgrade log4j 2.17.0 to mitigate yet another issue (#193)
    Co-authored-by: Nalini Ganapati <nalini@Ganapatis-MacBook-Pro-4.local>

--- scripts/prereqs/system/install_macos_prereqs.sh
+++ scripts/prereqs/system/install_macos_prereqs.sh
@@ -24,7 +24,7 @@
 set -e 
 
 # Install GenomicsDB Dependencies
-brew list cmake &>/dev/null && brew install cmake
+brew list cmake &>/dev/null || brew install cmake
 brew list mpich &>/dev/null || brew install mpich
 brew list ossp-uuid &>/dev/null || brew install ossp-uuid
 brew list libcsv &>/dev/null || brew install libcsv
