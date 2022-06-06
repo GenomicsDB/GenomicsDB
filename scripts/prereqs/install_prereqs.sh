c0a6a5224b1bafc5e924df600636c50e70418c75
nalinigans <nalini@omicsautomation.com>
2022-06-06 11:51:48 -0700

    Cleanup install_prereqs.sh

--- scripts/prereqs/install_prereqs.sh
+++ scripts/prereqs/install_prereqs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The MIT License (MIT)
-# Copyright (c) 2019-2021 Omics Data Automation, Inc.
+# Copyright (c) 2019-2022 Omics Data Automation, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
