8303333a62ed1c68c4a5ac1feb099854113539c6
mlathara <32438632+mlathara@users.noreply.github.com>
2022-06-06 10:51:26 -0700

    release automation (#207)

--- .github/scripts/tag_for_docker_release.sh
+++ .github/scripts/tag_for_docker_release.sh
@@ -6,12 +6,12 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [[ "$CURRENT_BRANCH" != "develop" || "$CURRENT_BRANCH" != "master" ]]; then
+if [[ "$CURRENT_BRANCH" != "develop" && "$CURRENT_BRANCH" != "master" ]]; then
     echo "Release should be run from develop or master branch!"
     exit 1
 fi
 
-VERSION=$(grep "GenomicsDB release verison" $REPO_ROOT/CMakeLists.txt | cut -d '"' -f 2)
+VERSION=$(grep "GenomicsDB release version" $REPO_ROOT/CMakeLists.txt | cut -d '"' -f 2)
 GIT_COMMIT_HASH=$(git log -1 --format=%h)
 
 TAG=v${VERSION}-${GIT_COMMIT_HASH}
