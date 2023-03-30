1f3c9f30d9b3eb7d9a395594c98559240d298d5f
eneskuluk <54481799+eneskuluk@users.noreply.github.com>
2023-03-30 08:28:14 -0700

    java17 workflow changes

--- .github/scripts/install_hadoop.sh
+++ .github/scripts/install_hadoop.sh
@@ -9,7 +9,7 @@ USER=`whoami`
 HADOOP=hadoop-${HADOOP_VER:-3.2.4}
 HADOOP_DIR=${INSTALL_DIR}/$HADOOP
 HADOOP_ENV=${HADOOP_ENV:-$HOME/hadoop_env.sh}
-JAVA_VER=${JAVA_VER:-8}
+JAVA_VER=${JAVA_VER:-17}
 
 install_prereqs() {
   # Java install in workflow yaml
