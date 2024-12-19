583815683ab8d0200d9d2dae9bfc050f856b92fd
Nalini Ganapati <35076948+nalinigans@users.noreply.github.com>
2024-12-19 09:27:54 -0800

    Expose more storage functionality as genomicsdb_utils api (#350)

--- src/main/cpp/src/api/genomicsdb_utils.cc
+++ src/main/cpp/src/api/genomicsdb_utils.cc
@@ -31,7 +31,7 @@
  **/
 #include "genomicsdb_utils.h"
 #include "tiledb_utils.h"
-
+#include "genomicsdb_logger.h"
 
 namespace genomicsdb {
 
@@ -39,23 +39,60 @@ std::string version() {
   return GENOMICSDB_VERSION;
 }
 
+#define LOG_ERRMSG if (strlen(tiledb_errmsg)) logger.error(tiledb_errmsg)
+
 bool is_file(const std::string& filename) {
-  return TileDBUtils::is_file(filename);
+  bool found =  TileDBUtils::is_file(filename);
+  if (!found) {
+    LOG_ERRMSG;
+  }
+  return found;
 }
 
 ssize_t file_size(const std::string& filename) {
-  return TileDBUtils::file_size(filename);
+  if (is_file(filename)) {
+    return TileDBUtils::file_size(filename);;
+  } else {
+    return GENOMICSDB_ERR;
+  }
 }
 
 int read_entire_file(const std::string& filename, void **buffer, size_t *length) {
-  return TileDBUtils::read_entire_file(filename, buffer, length);
+  if (is_file(filename)) {
+    return TileDBUtils::read_entire_file(filename, buffer, length);
+  } else {
+    return GENOMICSDB_ERR;
+  }
+}
+
+bool workspace_exists(const std::string& workspace) {
+  bool found = TileDBUtils::workspace_exists(workspace);
+  if (!found) {
+    LOG_ERRMSG;
+  }
+  return found;
+}
+
+bool array_exists(const std::string& workspace, const std::string& array_name) {
+  bool found = TileDBUtils::array_exists(workspace, array_name);
+  if (!found) {
+    LOG_ERRMSG;
+  }
+  return found;
 }
 
 std::vector<std::string> get_array_names(const std::string& workspace) {
-  return TileDBUtils::get_array_names(workspace);
+  auto names = TileDBUtils::get_array_names(workspace);
+  if (!names.size()) {
+    LOG_ERRMSG;
+  }
+  return names;
 }
 
 int cache_fragment_metadata(const std::string& workspace, const std::string& array_name) {
+  if (!TileDBUtils::array_exists(workspace, array_name)) {
+    return GENOMICSDB_ERR;
+  }
   return TileDBUtils::cache_fragment_metadata(workspace, array_name);
 }
 
